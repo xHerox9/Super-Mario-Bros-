bd6e6cb3e4555acbe8d73ebf011aa3c2f5b8024e
peterkang <peterkang@192.168.1.65>
2020-02-29 14:30:05 -0700

    Game with background

--- game/src/Game.java
+++ game/src/Game.java
@@ -3,11 +3,12 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Game extends Application {
-	private int HEIGHT = 600;
-	private int WIDTH = 600;
+	private int HEIGHT = 640;
+	private int WIDTH = 480;
 	private String GAME_TITLE = "Demo"; // temp title.
 	
 	public static void main(String[] args) {
@@ -19,10 +20,12 @@ public class Game extends Application {
 		Scene scene = new Scene (root);
 		Canvas canvas = new Canvas (HEIGHT,WIDTH);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
+		Image background = new Image("Map_design.png");
 		
 		root.getChildren().add(canvas);
 		primaryStage.setTitle(GAME_TITLE);
 		primaryStage.setScene(scene);
+		gc.drawImage(background, 0, 0);
 		primaryStage.show();
 	}
 }
