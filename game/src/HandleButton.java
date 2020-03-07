8b1b63f70b0f71a4b9c200daa900efcff643ad73
peterkang <peterkang@192.168.1.75>
2020-03-06 23:29:20 -0700

    Credit scene is completed

--- game/src/HandleButton.java
+++ game/src/HandleButton.java
@@ -6,6 +6,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HandleButton extends Game implements EventHandler<ActionEvent> {
@@ -41,17 +45,48 @@ public class HandleButton extends Game implements EventHandler<ActionEvent> {
 		GraphicsContext gc1 = canvas1.getGraphicsContext2D();
 		Image background1 = new Image("Map_design.png");
 		Button exit = new Button("Back");
+		Rectangle block = new Rectangle();
+		
 		exit.setLayoutX(550);
 		exit.setStyle("-fx-font-size: 2em;-fx-background-color: transparent;-fx-text-fill: black");
 		exit.setOnAction(e-> PStage.setScene(PScene));
+		
 		root1.getChildren().add(canvas1);
 		root1.getChildren().add(exit);
+		
 		if(type.equals("Play")){
-			System.out.println("you are playing");
+			System.out.println("Welcome to the game");
 		}
 		else {
 			gc1.drawImage(background1, 0, 0);
+			block.setLayoutX(70);
+			block.setLayoutY(100);
+			block.setWidth(500);
+			block.setHeight(330);
+			block.setFill(Color.ALICEBLUE);
+			root1.getChildren().add(block);
 		}
+		if(type.equals("Credit")) {
+			Text teamName = new Text(225, 120,"CPSC 233 TUT 7-02");
+			Text leader = new Text(180, 160,"Team Leader: Abhay Sharma");
+			Text member = new Text(155, 220,"Architect/Code Reviewer: Ehab Islam");
+			Text member1 = new Text(120, 280, "Artist/Second Programmer: Survat Kumra");
+			Text member2 = new Text(70, 340, "Game Designer/Lead Programmer: Muhammad Zahir");
+			Text member3 = new Text(170, 400, "Third Programmer: Peter Kang");
+			teamName.setFont(new Font(20));
+			leader.setFont(new Font(20));
+			member.setFont(new Font(20));
+			member1.setFont(new Font(20));
+			member2.setFont(new Font(20));
+			member3.setFont(new Font(20));
+			root1.getChildren().add(teamName);
+			root1.getChildren().add(leader);
+			root1.getChildren().add(member);
+			root1.getChildren().add(member1);
+			root1.getChildren().add(member2);
+			root1.getChildren().add(member3);
+		}
+			
 		
 		PStage.setScene(scene1);
 		
