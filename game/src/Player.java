bae1a43a7095c8f2379e17186a1313c24fddabae
ehabislam <59816228+ehabislam@users.noreply.github.com>
2020-04-06 12:49:11 -0600

    Update Player.java
    Done with victory mechanism

--- game/src/Player.java
+++ game/src/Player.java
@@ -14,16 +14,16 @@ public class Player extends Game implements EventHandler<KeyEvent> {
 	public int playRow;
 	public int playCol;
 	
-	public int[][] PlayerArray = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	public int[][] PlayerArray = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,6,6,0,0},
 			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 			 					 {0,5,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0},
 			 					 {1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1},
 			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0},
 			 					 {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
-			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0},
 			 					 {1,1,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,1},
-			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0},
 			 					 {1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
 			 					 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0},
 			 					 {0,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1},
@@ -33,6 +33,7 @@ public class Player extends Game implements EventHandler<KeyEvent> {
 	boolean A_check = false; // used to confirm if the A key has been pressed
 	boolean D_check = false; // used to confirm if the D key has been pressed
 	boolean W_check = false; // used to confirm if the W key has been pressed
+	boolean E_check = false; // used to confirm if the E key has been pressed
 	
 	public void updateGrid() {
 		for(int row = 0; row < PlayerArray.length; row++) {  // This for loop with go through the maps horizontally
@@ -104,7 +105,36 @@ public class Player extends Game implements EventHandler<KeyEvent> {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-
+					if(E_check == true) {
+						if(PlayerArray[row][column +1 ]==5 || PlayerArray[row][column-1] == 5) {
+							//System.out.println("Victory");
+							E_check = false;
+							if(PlayerArray[row][column +1 ]==5) {
+								PlayerArray[row][column +1 ] = 0;
+								for(int row1 = 0; row1 < PlayerArray.length; row1++) {  // This for loop with go through the maps horizontally
+									for (int column1 = 0; column1 < PlayerArray[row1].length; column1++) {
+										if(PlayerArray[row1][column1] == 6) {
+											PlayerArray[row1][column1] = 5;
+											break;
+										}
+									}
+								}
+								
+							}
+							if(PlayerArray[row][column -1 ]==5) {
+								PlayerArray[row][column -1 ] = 0;
+								for(int row1 = 0; row1 < PlayerArray.length; row1++) {  // This for loop with go through the maps horizontally
+									for (int column1 = 0; column1 < PlayerArray[row1].length; column1++) {
+										if(PlayerArray[row1][column1] == 6) {
+											PlayerArray[row1][column1] = 5;
+											break;
+										}
+									}
+								}
+								
+							}
+						}
+					}
 			        
 					
 				}
@@ -149,11 +179,26 @@ public class Player extends Game implements EventHandler<KeyEvent> {
 		switch (event.getCode()) {
 		case A: A_check = true; playerMain(); break; // In the situation that the A key has been pressed.
 		case D:	D_check = true; playerMain(); break; // In the situation that the D key has been pressed.
-		case W: W_check = true; playerMain(); break; // In the situation that the D key has been pressed.
+		case W: W_check = true; playerMain(); break; // In the situation that the W key has been pressed.
+		case E: E_check = true; playerMain(); break; // In the situation that the E key has been pressed.
 		default:
 			break; 
 		}
 	}
+	public boolean checkVictory() {
+		int counter = 0;
+//		for(int row = 0; row < PlayerArray.length; row++) {  // This for loop with go through the maps horizontally
+			for (int column = 0; column < PlayerArray[0].length; column++) { // This for loop will go through the map vertically
+				if(PlayerArray[0][column]==5) {
+					counter +=1;
+				}
+				if(counter == 3) {
+					System.out.println("Victory");
+					return true;
+				}
+			}	
+	//}
+	return false;
 
-
+	}
 }
