7a7541714934ccb6048f3b3a1a73fd27bf2bba5a
peterkang <peterkang@localhost>
2020-04-13 09:12:16 -0600

    Updated the game and handlebutton class with descriptive comments
    also added in stuff that got overwritten.

--- game/src/Game.java
+++ game/src/Game.java
@@ -7,56 +7,100 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+/*Class Game
+ * This program creates a game window with a main menu screen that has four button
+ * The instant variable for this class:
+ * HEIGHT: the height of the game window in pixels 
+ * WIDTH: the width of the game window in pixels 
+ * GAME_TITLE: the name of the game
+ */
+
 public class Game extends Application {
 	protected int HEIGHT = 480;
 	
 	protected int WIDTH = 640;
-	private String GAME_TITLE = "Laggy Luigi"; // temp title.
-//	private HashMap<KeyCode, Boolean> keys= new HashMap<KeyCode , Boolean>();
+	private String GAME_TITLE = "Laggy Luigi";
+	
+/* main (String[] args)
+ * The main method of the class
+ * Used to launch the program in javafx
+ * @param String[] args
+ */
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
-	
+/* start(Stage primaryStage)
+ * The start method, what the program should do when it is launch 
+ * @param Stage primaryStage: a stage object/ window for objects to be place on
+ * @throws Exception
+ */
 	public void start(Stage primaryStage) throws Exception {
-		// code for the initialization of stage and scene
 		
+		/*The following block of code is for the initialization of the stage (the game window) and the scene (main menu screen)
+		 * The variables:
+		 * window: which is the primaryStage/ game window put in 
+		 * root: used to add nodes in (like a contain) 
+		 * canvas: used to draw things on the scene 
+		 * gc: used with the canvas to draw 2d objects
+		 * background: used to get the main menu screen background 
+		 */
 		Stage window = primaryStage;
 		Group root = new Group();
 		Scene scene = new Scene (root);
 		Canvas canvas = new Canvas (WIDTH,HEIGHT);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		Image background = new Image("starting_screen.jpg");
+		Image background = new Image("starting_screen.jpg"); // from: https://unsplash.com/photos/Xaanw0s0pMk and https://www.hiclipart.com/search?clipart=Luigi 
 		
-		// code for initialization of button for menu
+		/*The following block of code is for the initialization of the four buttons for the main menu screen 
+		 * There four buttons: play, about, how to play and credit
+		 */
 		Button play = createPlayButton();
 		Button about = createAboutButton();
 		Button howTo = createHowToPlayButton();
 		Button credit= createCreditButton();
 		
-		// code for action listener for when button is pressed
+		/*The following block of code is for the setting of an action for the four buttons 
+		 * This will pass arguments into the Handlebutton class
+		 */
 		play.setOnAction(new HandleButton(window, scene, "Play"));
 		about.setOnAction(new HandleButton(window, scene, "About"));
 		howTo.setOnAction(new HandleButton(window, scene, "HowTo"));
 		credit.setOnAction(new HandleButton(window, scene,"Credit"));
 		
-		//adding stuff onto  main scene
+		/*The following block of code adds the canvas and the four buttons into the main menu screen
+		 * This is done by adding each node into the root
+		 */
 		root.getChildren().add(canvas);
 		root.getChildren().add(play);
 		root.getChildren().add(about);
 		root.getChildren().add(howTo);
 		root.getChildren().add(credit);
 		
-		//setting up the properties of the stage
+		/*The following block of code is setting up the properties of the game window
+		 * It displays the game title on the game window 
+		 * It set the main menu scene on to the stage
+		 * It draws the main menu screen background onto the stage 
+		 * It set the game window as non adjustable 
+		 * It makes the game window visible 
+		 * It prints out a message "Good Bye" onto the console when the game window is close
+		 */
 		window.setTitle(GAME_TITLE);
 		window.setScene(scene);
 		gc.drawImage(background, 0, 0);
 		window.setResizable(false);
 		window.show();
-		//code for message when window is closed
 		window.setOnCloseRequest(e-> System.out.println("Good Bye"));
 	}
-	// code for creating the Play button
+	
+	/*createPlayButton()
+	 * The method for the creation of the Play button 
+	 * Labels the button "Play"
+	 * Sets the starting pixel coordinate of the button at 50, 200
+	 * Sets the size of the button to be 100 by 50 pixels 
+	 * Sets the background of the button to be light blue
+	 * @return button with set properties 
+	 */
 	public Button createPlayButton() {
 		Button play = new Button("Play");
 		play.setLayoutX(50);
@@ -65,7 +109,15 @@ public class Game extends Application {
 		play.setStyle("-fx-background-color: #87ceeb");
 		return play; 
 	}
-	// code for creating the About button
+	
+	/*createAboutButton()
+	 * The method for the creation of the About button 
+	 * Labels the button "About"
+	 * Sets the starting pixel coordinate of the button at 50, 100
+	 * Sets the size of the button to be 100 by 50 pixels 
+	 * Sets the background of the button to be light blue
+	 * @return button with set properties 
+	 */
 	public Button createAboutButton() {
 		Button about= new Button("About");
 		about.setLayoutX(50);
@@ -75,7 +127,14 @@ public class Game extends Application {
 		return about;
 	}
 	
-	//code for creating the How to Play button
+	/*createHowToPlayButton()
+	 * The method for the creation of the How to Play button 
+	 * Labels the button "How to Play"
+	 * Sets the starting pixel coordinate of the button at 50, 300
+	 * Sets the size of the button to be 100 by 50 pixels 
+	 * Sets the background of the button to be light blue
+	 * @return button with set properties 
+	 */
 	public Button createHowToPlayButton() {
 		Button howTo = new Button ("How to Play");
 		howTo.setLayoutX(50);
@@ -86,7 +145,14 @@ public class Game extends Application {
 		
 	}
 	
-	//code for creating the Credit button
+	/*createCreditButton()
+	 * The method for the creation of the Credit button 
+	 * Labels the button "Credit"
+	 * Sets the starting pixel coordinate of the button at 50, 400
+	 * Sets the size of the button to be 100 by 50 pixels 
+	 * Sets the background of the button to be light blue
+	 * @return button with set properties 
+	 */
 	public Button createCreditButton() {
 		Button credit= new Button("Credit");
 		credit.setLayoutX(50);
--- game/src/HandleButton.java
+++ game/src/HandleButton.java
@@ -1,7 +1,5 @@
-//Handles the button on the game main screen
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -17,28 +15,40 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-
-//No parameters
-//handles event on button presses
+/*Class HnadleButton
+ * This class deals with what happens when each of the four button on the menu main screen is pressed 
+ * The instant variable for this class are:
+ * PStage: the primaryStage/ game window
+ * PScene: the scene for the buttons 
+ * type: the ID/tag of each button
+ */
 public class HandleButton extends Game implements EventHandler<ActionEvent>{
-	//instance variables 
 	private Stage PStage;
 	private Scene PScene;
 	private String type;	
-	//Constructor
+
+	/*HandleButton(Stage window, Scene scene, String type)
+	 * The constructor of the HandleButton class
+	 * @param Stage window: the game window 
+	 * @param Scene scene: the scene
+	 * @param String type: the ID/tag of the button pressed
+	 */
 	public HandleButton(Stage window, Scene scene, String type) {
 		this.PStage= window;
 		this.PScene= scene;
 		this.type= type;
 		
 	}
-	//Copy constructor
+	/*HandleButton(HandleButton obj)
+	 * The copy constructor of the HandleButton class
+	 * @param HandleButton obj: the object of the HandleButton
+	 */
 	HandleButton(HandleButton obj){
 		PStage= obj.PStage;
 		PScene= obj.PScene;
 		type= obj.type;
 	}
-	//getters
+	// the getters for the PScene, PStage and type 
 	public Scene getPScene() {
 		return PScene;
 	}
@@ -49,19 +59,33 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 	public String getType() {
 		return type;
 	}
-	// handle method
+	/*Handle(ActionEvent event)
+	 * The handle method, what to do when button is pressed
+	 * @param ActionEvent event: the pressed button event
+	 */
 	public void handle(ActionEvent event) {
-		// initialization of new scene
+		/*The following block of code is for the initialization of the new scene for each of the button
+		 * root1: used for adding nodes in 
+		 * scene1: the scene for the button
+		 * canvas1: used to draw on the game window
+		 * gc1: used with canvas to draw in 2D
+		 * background1: get the background image for the buttons 
+		 */
 		Group root1 = new Group();
 		Scene scene1 = new Scene (root1);
 		Canvas canvas1 = new Canvas (WIDTH,HEIGHT);
 		GraphicsContext gc1 = canvas1.getGraphicsContext2D();
 		Image background1 = new Image("backgroundmain.png");
 		
-		//initialization of back button 
+		/*This line of code is for the initialization of a back button 
+		 * The back button is used when the user wants to get back to the main menu screen when they are at
+		 * the About or How to Play or Credit screen
+		 */
 		Button back = createBackButton();
 		
-		//initialization of text for credit button
+		/*The following block of code is for the initialization of the text that will be placed on to the credit screen
+		 * First text is the team name = and the following text are for the members of the team
+		 */
 		Text teamName = creditTeamText();
 		Text leader = creditLeaderText();
 		Text member = creditMemberText();
@@ -69,13 +93,13 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 		Text member2 = creditMember2Text();
 		Text member3 = creditMember3Text();
 		
-		//initialization of text for about button
+		// This line of code is for the initialization of the text to be place on the About screen 
 		Text about = aboutText();
 		
-		//initialization of text for the How To Play button
+		// This line of code is for the initialization of the text to be placed on the How to Play screen
 		Text howTo = howToText();
 		
-		//adding things to scene
+		// The following two lines of code is for the addition of the canvas and back button onto the button screen 
 		root1.getChildren().add(canvas1);
 		root1.getChildren().add(back);
 		
@@ -148,12 +172,21 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 			PStage.setScene(playScene);
 			PStage.show();
 		}
-		//else statement will make new scene when button other than play is pressed
+		/*The following block of code is for the else statement when a button is pressed other than the Play button
+		 * (About, How to Play and Credit)
+		 */
 		else {
+			/*The following  three lines of code is for:
+			 * the drawing of the background for each of the button 
+			 * initialization of a rectangle block object 
+			 * adding the rectangle block object onto the button screen 
+			 */
 			gc1.drawImage(background1, 0, 0);
 			Rectangle block = createRectangle();
 			root1.getChildren().add(block);
-			// if statement for when the credit button is press, it will display text specific to the credit button
+			/*The following block of code is for the IF statement when the Credit button is pressed
+			 * It will add the text that pertain to the credit screen, team name and the members of the team 
+			 */
 			if(type.equals("Credit")) {
 				root1.getChildren().add(teamName);
 				root1.getChildren().add(leader);
@@ -162,11 +195,29 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 				root1.getChildren().add(member2);
 				root1.getChildren().add(member3);
 			}
-			// else if statement for when the about is press, it will display text specific to the about button
+			/*The following line of code is for the ELSE IF statement when the About button is pressed
+			 * It will add the text that pertain to the about screen
+			 */
 			else if(type.equals("About")) {
 				root1.getChildren().add(about);
 			
 			}
+			/*The following block of code is for the ELSE statement for a button other than the About and Credit that is pressed
+			 * (How to Play button)
+			 * gets the image to be display on the screen
+			 * allows the image to be viewed
+			 * set the height and width of the image to 200 by 200 pixels
+			 * set the starting pixel coordinate to 370,100 for the image
+			 * gets another image to be display on the screen
+			 * allows the image to be viewed
+			 * set the height and width of the image to 25 by 25 pixels
+			 * set the starting pixel coordinate to 455,165 for the image
+			 * gets another image to be display on the screen
+			 * allows the image to be viewed
+			 * set the height and width of the image to 25 by 25 pixels
+			 * set the starting pixel coordinate to 455,165 for the image
+			 * adds the text that pertain to the How to Play screen and all of the images for display
+			 */
 			else if(type.equals("HowTo")) {
 				Image keys = new Image ("keys.gif"); // gif from: https://dribbble.com/shots/4955867-Key-Lime-Pie
 				ImageView keyView = new ImageView (keys);
@@ -174,18 +225,40 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 				keyView.setFitWidth(200);
 				keyView.setX(370);
 				keyView.setY(100);
+				Image x = new Image ("x.png"); // from: http://clipart-library.com/clipart/rcLxML7Ri.htm
+				ImageView xView = new ImageView(x);
+				xView.setFitHeight(25);
+				xView.setFitWidth(25);
+				xView.setX(455);
+				xView.setY(165);
+				Image e = new Image ("e.png"); // from: https://www.iconsdb.com/orange-icons/letter-e-icon.html
+				ImageView eView = new ImageView (e);
+				eView.setFitHeight(25);
+				eView.setFitWidth(25);
+				eView.setX(455);
+				eView.setY(165);
 				root1.getChildren().add(howTo);
 				root1.getChildren().add(keyView);
+				root1.getChildren().add(xView);
+				root1.getChildren().add(eView);
 			
 			}
-			// put the scene on the window
-			// this scene is for only button other than the play button
+			/*This line of code for the setting the scene onto the game window for button other than the Play button
+			 * (About, How to Play and Credit)
+			 */
 			PStage.setScene(scene1);
 		}
 		
 	}
 	
-	// following code is for the creation of text for credit button
+	/*createBackButton()
+	 * The method to create the back button
+	 * labels the button "Back"
+	 * sets the button at a starting x pixel coordinate of 550
+	 * sets the background of the button to be transparent and fill the text to be black
+	 * set an action that if the button is pressed go back to the main menu screen
+	 * @return button set with properties 
+	 */
 	public Button createBackButton() {
 		Button back = new Button("Back");
 		back.setLayoutX(550);
@@ -193,6 +266,14 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 		back.setOnAction(e-> PStage.setScene(PScene));
 		return back;
 	}
+	/*createRectangle()
+	 * The method to create a rectangle object
+	 * No label for the rectangle
+	 * set the starting pixel coordinate to 70, 100
+	 * set the width and height of the rectangle to 500 by 330
+	 * fill the rectangle to a light blue
+	 * @return block with set properties 
+	 */
 	public Rectangle createRectangle() {
 		Rectangle block = new Rectangle();
 		block.setLayoutX(70);
@@ -202,40 +283,82 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 		block.setFill(Color.ALICEBLUE);
 		return block;
 	}
+	/* creditTeamText()
+	 * The method for the creation of the team name text
+	 * sets the starting pixel coordinate to 225, 120 and writes the text
+	 * set the font to size 20
+	 * @return text
+	 */
 	public Text creditTeamText() {
 		Text teamName = new Text(225, 120,"CPSC 233 TUT 7-02");
 		teamName.setFont(new Font(20));
 		return teamName;
 	}
+	/* creditLeaderText()
+	 * The method for the creation of the leader text
+	 * sets the starting pixel coordinate to 180, 160 and writes the text
+	 * set the font to size 20
+	 * @return text
+	 */
 	public Text creditLeaderText() {
 		Text leader = new Text(180, 160,"Team Leader: Abhay Sharma");
 		leader.setFont(new Font(20));
 		return leader;
 	}
+	/* creditMemberText()
+	 * The method for the creation of the member text
+	 * sets the starting pixel coordinate to 155, 220 and writes the text
+	 * set the font to size 20
+	 * @return text
+	 */
 	public Text creditMemberText() {
 		Text member = new Text(155, 220,"Architect/Code Reviewer: Ehab Islam");
 		member.setFont(new Font(20));
 		return member;
 	}
+	/* creditMemberText()
+	 * The method for the creation of the member text
+	 * sets the starting pixel coordinate to 120, 280 and writes the text
+	 * set the font to size 20
+	 * @return text
+	 */
 	public Text creditMember1Text() {
 		Text member1 = new Text(120, 280, "Artist/Second Programmer: Survat Kumra");
 		member1.setFont(new Font(20));
 		return member1;
 	}
+	/* creditMemberText()
+	 * The method for the creation of the member text
+	 * sets the starting pixel coordinate to 70, 340 and writes the text
+	 * set the font to size 20
+	 * @return text
+	 */
 	public Text creditMember2Text() {
 		Text member2 = new Text(70, 340, "Game Designer/Lead Programmer: Muhammad Zahir");
 		member2.setFont(new Font(20));
 		return member2;
 	}
+	/* creditMemberText()
+	 * The method for the creation of the member text
+	 * sets the starting pixel coordinate to 170, 400 and writes the text
+	 * set the font to size 20
+	 * @return text
+	 */
 	public Text creditMember3Text() {
 		
 		Text member3 = new Text(170, 400, "Third Programmer: Peter Kang");
 		member3.setFont(new Font(20));
 		return member3;
 	}
-	// code for text for the about scene
+	/*aboutText()
+	 * The method for the creation of the about text
+	 * The instant variable of text: text/story to display
+	 * set the starting pixel coordinate at 80, 150
+	 * set the font to size 12
+	 * @return text with set properties 
+	 */
 	public Text aboutText() {
-		String text = "Having been fed up with his brother, MarioÃ¢â‚¬â„¢s popularity and his dwindling popularity.\n"+ 
+		String text = "Having been fed up with his brother, Mario™ popularity and his dwindling popularity.\n"+ 
 				"Luigi is a man on a mission to reclaim his dominance as being the best brother. No\n" +
 				"more shall he be only the MC of a haunted house game. Screw to always being\n"+
 				"overshadowed by Mario in every game they appear in. The time has come for action!!\n"+
@@ -247,11 +370,20 @@ public class HandleButton extends Game implements EventHandler<ActionEvent>{
 		about.setFont(new Font(12));
 		return about;
 		}
+	/*howToText()
+	 * The method for the creation of the text for the How to Play screen
+	 * The instant variable of text: the instruction 
+	 * set the starting pixel coordinate to 80, 150
+	 * set the font to size 30
+	 * @return text with set properties 
+	 */
 	public Text howToText() {
 		String text = 
 				"Instruction\n"+
 				"D: to move right\n"+
-				"A to move left\n"+"w to jump";
+				"A: to move left\n"+
+				"W: to jump\n"+
+				"E: to pick up stars";
 		Text howTo = new Text(80,150,text);
 		howTo.setFont(new Font(30));
 		return howTo;
